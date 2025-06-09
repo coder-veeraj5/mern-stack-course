601afee692354a23f5db02eb0ce36c62ee983309
veeraj <veeraj@123.com>
2025-06-09 20:30:39 +0530

    cg

--- 2month/index.js
+++ 2month/index.js
@@ -1,1 +1,10 @@
-console.log("VEERAJ PATIL")
+console.log("VEERAJ PATIL");
+let f="veeraj"
+let m="vijykumar "
+let l="patil"
+
+function myfunction(f,m,l){
+    console.log(f+m+l);
+}
+
+console.log(myfunction(f,m,l));

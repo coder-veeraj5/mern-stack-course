cab569add3b9d3ec318af3a1b1fe414917ad4665
veeraj <veeraj@123.com>
2025-06-11 20:33:22 +0530

    done

---
+++ 2month/javascript/index2.js
@@ -0,0 +1,18 @@
+let a="veeraj";
+var b="patil";
+const pi=3.14;
+let c=9;
+let d=10;
+
+let obj={
+    "name":"veeraj",
+    "salary":"100corores",
+}
+
+console.log(obj);
+
+const array=["veeraj" ,"yash" , "sahil"];
+
+function addd(c,d){
+    return a+b;
+}

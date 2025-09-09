24bf99514fbde33d24b7076b3d62bdf45016bceb
veeraj <veeraj@123.com>
2025-09-09 19:07:10 +0530

    done

--- 4september/javascript/6imideateinvokefun.js
+++ 4september/javascript/6imideateinvokefun.js
@@ -1,12 +1,12 @@
 
-// immidiate invoke 
+// immidiate invoke ==> IIFE is to create a private scope
 
  (function(){
     console.log("hello veeraj");
     
  })();
 
-
+//self invoke 
  
  (function(){
     console.log("hello yash");
---
+++ 4september/javascript/7destructuringobject.js
@@ -0,0 +1,22 @@
+ 
+    const obj={
+        name: "veeraj",
+        surname:"patil",
+        obj2:{
+       
+            age:16,
+        }
+    }
+    const {name:naam,surname}=obj; 
+    console.log(naam,surname);
+
+    const {age}=obj.obj2; 
+    console.log(age);
+    
+    
+    const arr=[2,4,6,8,10];
+
+    const [a,b,c]=arr;
+    console.log(a,b,c);
+    
+    
